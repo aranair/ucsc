0440015212c618caaad85b2b2f9e39400beaccc4
aranair <boa.homan@gmail.com>
2020-11-03 10:09:26 -0500

    Fix

--- components/buttonLink.js
+++ components/buttonLink.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Link from 'next/link'
 import Button from '@material-ui/core/Button'
 
-const ButtonLink = ({ className, href, hrefAs, children }) => (
-  <Link href={href} as={hrefAs} passHref>
-    <a className={className}>
-      {children}
-    </a>
-  </Link>
-)
+const ButtonLink = ({ className, href, hrefAs, children }) => {
+  const buildLocal = process.env.NEXT_PUBLIC_BUILD_LOCAL;
+  const url = href[0] === '/' && href !== '/' && buildLocal === 'FALSE' ? `${href}.html` : href;
+
+  return (
+    <Link href={url} as={hrefAs} passHref>
+      <a className={className}> {children} </a>
+    </Link>
+  )
+}
 
 export default ButtonLink
--- components/menuLink.js
+++ components/menuLink.js
@@ -3,12 +3,17 @@ import Link from 'next/link'
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
-const MenuLink = ({ text, className, href, hrefAs, children, onClick }) => (
-  <Link href={href} as={hrefAs} passHref>
-    <ListItem button component="a" onClick={onClick}>
-      <ListItemText>{text}</ListItemText>
-    </ListItem>
-  </Link>
-)
+const MenuLink = ({ text, className, href, hrefAs, children, onClick }) => {
+  const buildLocal = process.env.NEXT_PUBLIC_BUILD_LOCAL;
+  const url = href[0] === '/' && href !== '/' && buildLocal === 'FALSE' ? `${href}.html` : href;
+
+  return (
+    <Link href={url} as={hrefAs} passHref>
+      <ListItem button component="a" onClick={onClick}>
+        <ListItemText>{text}</ListItemText>
+      </ListItem>
+    </Link>
+  )
+}
 
 export default MenuLink
